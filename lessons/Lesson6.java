331f57252530d004a71bc12de1f2890ccc6ce2e2
fiona fung <175372480+ffung-dev@users.noreply.github.com>
2024-09-21 14:21:20 -0400

    Create Lesson6.java
    copy/paste

---
+++ lessons/Lesson6.java
@@ -0,0 +1,14 @@
+//***********************************************
+// Lesson6.java  Author: Fiona Fung
+//
+// First program: print two lines to the screen
+//***********************************************
+
+public class Lesson6 //class definition
+{
+   public static void main (String[] args) // main method
+   {
+      System.out.println ("Java is fun!"); // print statement, error was "println" not "printIn"
+      System.out.println ("So this guy in the front of the room says...");
+   }
+}
