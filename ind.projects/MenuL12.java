573b1e406105a759e160aba4e9e29b4488991528
Fiona Fung <ffung.dev@gmail.com>
2024-09-27 20:48:57 -0400

    upload MenuL12.java, fix Calculator.java

---
+++ ind.projects/MenuL12.java
@@ -0,0 +1,33 @@
+//****************************************************
+// MenuL12.java            Author: Fiona Fung
+//
+// Takes food orders from user
+// Includes: if/else, while, arrays, methods
+//****************************************************
+
+import java.util.*;
+
+public class MenuL12 
+{
+   static void menuItems()
+   {
+      // creates variables for menu items
+      String[] main = {"pineapple bun", "buttered french toast", "fish 'n chips"};
+      String[] dessert = {"cinnamon roll", "macaron", "cupcake", "cheesecake"};
+      String[] drinks = {"caramel latte", "hot chocolate", "peppermint tea", "cappuccino", "milk tea"};
+   }
+   
+   static void menuMethod() 
+   {
+      // a function that displays menu
+      
+      
+   }
+   
+   public static void main (String[] args)
+   {
+   
+   
+   }
+   
+}
