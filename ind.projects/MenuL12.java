5f64b2a31b4cc5fc04ad283f52b1cb5844447176
Fiona Fung <ffung.dev@gmail.com>
2024-09-30 18:34:09 -0400

    edited MenuL12.java, changed methods

--- ind.projects/MenuL12.java
+++ ind.projects/MenuL12.java
@@ -2,32 +2,31 @@
 // MenuL12.java            Author: Fiona Fung
 //
 // Takes food orders from user
-// Includes: if/else, while, arrays, methods
+// Includes: if/else, while, arrays, methods, parameters
 //****************************************************
 
 import java.util.*;
 
 public class MenuL12 
 {
-   static void menuItems()
-   {
-      // creates variables for menu items
-      String[] main = {"pineapple bun", "buttered french toast", "fish 'n chips"};
-      String[] dessert = {"cinnamon roll", "macaron", "cupcake", "cheesecake"};
-      String[] drinks = {"caramel latte", "hot chocolate", "peppermint tea", "cappuccino", "milk tea"};
+   static void menuList (String dish, String price) { // parameters = variables in methods
+      System.out.println(dish + " - $" + price);
    }
    
    static void menuMethod() 
    {
+      // creates variables for menu items
+      String[] main = {"pineapple bun", "buttered french toast", "fish 'n chips", "rice noodles"};
+      String[] dessert = {"cinnamon roll", "macaron", "cupcake", "cheesecake"};
+      String[] drinks = {"caramel latte", "hot chocolate", "peppermint tea", "cappuccino"};
       // a function that displays menu
-      
+      menuList(main[1], "3.50");
       
    }
    
    public static void main (String[] args)
    {
-   
-   
+      menuMethod();
    }
    
 }
