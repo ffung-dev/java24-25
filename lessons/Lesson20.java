99711163257651a49045c0211b24f5c9141badf8
ffung-dev <ffung.dev@gmail.com>
2025-01-16 16:59:24 -0500

    round up with Math.ceil

--- lessons/Lesson20.java
+++ lessons/Lesson20.java
@@ -16,7 +16,7 @@ public class Lesson20
          System.out.println("error: second number is less than the first");
          return 0;
       } else {
-         for (int i = a; i <= b; i++)
+         for (int i = (int)(Math.ceil(a)); i <= b; i++)
          {
             sum += i;
          }
@@ -32,7 +32,7 @@ public class Lesson20
          System.out.println("error: second number is less than the first");
          return 0;
       } else {
-         for (int i = (int)(a); i < b ; i++)
+         for (int i = (int)(Math.ceil(a)); i <= b ; i++)
          {
             sum += i;
          }
@@ -43,15 +43,35 @@ public class Lesson20
    public static void main(String[] args)
    {
       Scanner input = new Scanner(System.in);
-      System.out.println("do you want to input integers or floating-point numbers?\n1 = integers\n0 = floating-point numbers");
-      int choice = input.nextInt();
-      if (choice == 1)
+      boolean valid = true;
+      
+      do
       {
-      }
-      System.out.println("enter first range number: ");
-      double a = input.nextDouble();
-      System.out.println("enter second range number (must be greater than the first): ");
-      double b = input.nextDouble();
-      System.out.println("sumRange: " + sumRange(a, b));
+         System.out.println("do you want to input integers or floating-point numbers?\n1 = integers\n0 = floating-point numbers");
+         int choice = input.nextInt();
+         if (choice == 1)
+         // integers
+         {
+            System.out.println("enter first range number: ");
+            int a = input.nextInt();
+            System.out.println("enter second range number (must be greater than the first): ");
+            int b = input.nextInt();
+            System.out.println("sumRange: " + sumRange(a, b));
+            valid = true;
+         } else if (choice == 0) {
+         // doubles
+            System.out.println("enter first range number: ");
+            double a = input.nextDouble();
+            System.out.println("enter second range number (must be greater than the first): ");
+            double b = input.nextDouble();
+            System.out.println("sumRange: " + sumRange(a, b));
+            valid = true;
+         } else {
+            valid = false;
+            System.out.println("error: did not input valid option (1/0)");      
+         }
+      } while (!valid);
    }
+   
 }
+
