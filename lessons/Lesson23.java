bde58c5bf06c190113da7bc2ae2af88054001dca
ffung-dev <ffung.dev@gmail.com>
2025-01-27 15:47:26 -0500

    create Lesson23.java

---
+++ lessons/Lesson23.java
@@ -0,0 +1,18 @@
+//*************************************************** 
+public class Lesson23 
+{
+    public static void main(String[]args)
+    {
+        BankTeller green = new BankTeller("Mr. Green", "5D3AL0388");
+        Branch newClue = new Branch("Green Bay", "Green Bay", 3001000, green);
+        Bank clue = new Bank("Clue Bank", newClue);
+        CustomerAccount scarlett = new CustomerAccount("Scarlett Red", 600600, 555, 1000, null);
+        
+        System.out.println(clue.toString());
+        System.out.println(newClue.toString());
+        System.out.println(green.toString());
+        System.out.println(scarlett.toString());
+        
+
+    }
+}
