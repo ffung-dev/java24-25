c57e368bd5e5191b1780e254bd8bfa5ba3fcebb5
ffung-dev <ffung.dev@gmail.com>
2025-01-27 15:47:19 -0500

    create CustomerAccount.java

---
+++ lessons/CustomerAccount.java
@@ -0,0 +1,38 @@
+//********************************************************
+//     CustomerAccount.java        Author: Fiona Fung
+//
+//  provides information about a Bank customer
+//********************************************************
+
+public class CustomerAccount {
+    // instance variables
+    String name, accountType;
+    int accountNum, IDNum;
+    double bal;
+    
+    // constructor
+    public CustomerAccount(String name, int accountNum, int IDNum, double bal, String accountType)
+    {
+        this.name = name;
+        this.accountNum = accountNum;
+        this.IDNum = IDNum;
+        this.bal = bal;
+        this.accountType = accountType;
+    }
+
+    // get methods
+    public String getName() { return name; }
+    public int getAccountNum() { return accountNum; }
+    public int getIDNum() { return IDNum; }
+    public double getBal() { return bal; }
+    public String getAccountType() { return accountType; }
+
+    // set methods
+    public void setName(String name) { this.name = name; }
+    public void setAccountNum(int accountNum) { this.accountNum = accountNum; }
+    public void setIDNum(int IDNum) { this.IDNum = IDNum; }
+    public void setBal(double bal) { this.bal = bal;}
+    public void setAccountType(String accountType) { this.accountType = accountType; }
+
+    
+}
