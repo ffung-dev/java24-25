2953a9f1ef098d2aced63e0e90f6f099f94fb79e
ffung-dev <ffung.dev@gmail.com>
2025-02-10 17:23:39 -0500

    created ClickerGame.java

---
+++ ind.projects/ClickerGame.java
@@ -0,0 +1,16 @@
+//*******************************************
+//  ClickerGame.java        fiona fung
+//
+//  clicker game ;)
+//*******************************************
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Scanner;
+
+public class ClickerGame {
+    public static void main(String[] args) {
+        // create stuff
+        Scanner input = new Scanner(System.in);
+    }
+}
