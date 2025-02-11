9eba0b8441ae44076d736f0f199c315c367e4351
ffung-dev <ffung.dev@gmail.com>
2025-02-11 17:18:46 -0500

    added basic panel & button

--- ind.projects/ClickerGame.java
+++ ind.projects/ClickerGame.java
@@ -6,11 +6,50 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.util.Scanner;
 
 public class ClickerGame {
+
+    // variables outside main method > access anywhere in class
+    private static ClickerGame obj;
+    private static int score = 0;
+    private static JLabel scoreLabel = new JLabel(" clicks : " + score);
+
+
+    public ClickerGame() { obj = this; }
     public static void main(String[] args) {
-        // create stuff
+        new ClickerGame();
+        // create variables & game things
         Scanner input = new Scanner(System.in);
+        String name;
+
+        // set jframe, jpanel, jbutton stuff
+        JFrame frame = new JFrame(" clicker game ");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.pink);
+        
+        JButton button = new JButton(" click me ");
+        button.setBackground(Color.white);
+        button.addActionListener(obj.new MyListener());
+
+
+        frame.add(panel);
+        panel.add(button);
+        panel.add(scoreLabel);
+
+        frame.setVisible(true);
+        input.close();
+    }
+
+    private class MyListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            score++;
+            scoreLabel.setText(" clicks : " + score);
+        }
     }
 }
